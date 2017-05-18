24a134abecf5119bb1ded6f2e40f50f8bc1504cc
grevillopes <grevillopes@gmail.com>
2017-05-18 12:43:47 -0500

    Added jQuery functionality for grabbing results from GitHub API

--- js/index.js
+++ js/index.js
@@ -1,33 +1,114 @@
-/* index.js */
-"use strict";
-var msg = "hello JavaScript";
-console.log(msg);
+/*
+ index.js
+ */
 
-var resultDiv = document.getElementById("results");
-resultDiv.innerHTML = "<p> This is from JavaScript</p>";
+$(document).ready(function () {
 
-console.log("msg is " + typeof(msg));
-console.log("resultDiv is " + typeof(resultDiv));
+    "use strict";
+// var msg = "hello JavaScript";
+// console.log(msg);
+//
+// var resultDiv = document.getElementById("results");
+// resultDiv.innerHTML = "<p> This is from JavaScript</p>";
 
-var none;
-console.log("none is " + typeof(none));
+    var resultList = $('#resultList');
+    resultList.text("This is from jQuery");
 
-var aNumber = 0;
-console.log("aNumber is " + typeof(aNumber));
+    var toggleButton = $("#toggleButton");
+    toggleButton.on("click", function () {
+        resultList.toggle(500);
 
-var trueFalse = true;
-console.log("trueFalse is " + typeof(trueFalse));
+        if (toggleButton.text() == "Hide") toggleButton.text("Show");
+        else toggleButton.text("Hide");
 
-var Hello="null";
-console.log("Hello is " + typeof(Hello));
+    });
 
-if(!none){
-    console.log("none is undefined");
-}
+    var listItems = $("header nav li");
+    listItems.css("font-weight", "bold");
+    listItems.filter(":first").css("font-size", "18px");
 
-if (aNumber == "0"){
-    console.log("10 is 10")
-}
 
-function showMsg(msg)
+// console.log("msg is " + typeof(msg));
+// console.log("resultDiv is " + typeof(resultDiv));
+//
+// var none;
+// console.log("none is " + typeof(none));
+//
+// var aNumber = 0;
+// console.log("aNumber is " + typeof(aNumber));
+//
+// var trueFalse = true;
+// console.log("trueFalse is " + typeof(trueFalse));
+//
+// var Hello="null";
+// console.log("Hello is " + typeof(Hello));
+//
+// if(!none){
+//     console.log("none is undefined");
+// }
+//
+// if (aNumber == "0"){
+//     console.log("10 is 10")
+// }
+//
+// function showMsg(msg)
+//
 
+    var gitHubSearch = "https://api.github.com/search/repositories?q=jquery+language:javascript&sort=stars";
+    $.get(gitHubSearch, function(r){
+        //console.log(r.items.length);
+        displayResults(r.items);
+    });
+
+
+    // var results = [{
+    //     name: "jQuery",
+    //     language: "JavaScript",
+    //     score: 4.5,
+    //     showLog: function () {
+    //
+    //     },
+    //     Owner: {
+    //         login: "GrevilLopes",
+    //         id: 12345
+    //     }
+    // },{
+    //     name: "jQuery UI",
+    //     language: "JavaScript",
+    //     score: 3.5,
+    //     showLog: function () {
+    //
+    //     },
+    //     Owner: {
+    //         login: "GrevilLopes",
+    //         id: 12345
+    //     }
+    // }];
+
+    function displayResults(results){
+    resultList.empty();
+    $.each(results, function(i, item){
+
+        var newResult = $("<div class='result'>" +
+                "<div class='title'>" + item.name + "</div>" +
+                "<div>Language: " + item.language + "</div>" +
+                "<div>Full name: " + item.full_name + "</div>" +
+                "</div>");
+
+        newResult.hover(function () {
+            //make it darker
+            $(this).css("background-color", "lightgray");
+        }, function(){
+            //make it lighter
+            $(this).css("background-color", "transparent");
+        })
+
+        resultList.append(newResult);
+
+
+    });
+    }
+
+
+
+});
